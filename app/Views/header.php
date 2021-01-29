ed416817922168bd99139a62cdb62d3fc5411fa2
maheshrapelli141 <maheshrapelli141@gmail.com>
2021-01-29 19:31:43 +0530

    basic seo done

--- app/Views/header.php
+++ app/Views/header.php
@@ -7,33 +7,20 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Blend &mdash; Free HTML5 Bootstrap Website Template by FreeHTML5.co</title>
+	<title>Jeet Props &mdash; The one stop ,prop shop</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FreeHTML5.co" />
+	<meta name="description" content="Setup for films, television,Ad's, events, wedding and many more" />
+	<meta name="keywords" content="films,television,Ad's, events, wedding" />
+	<meta name="author" content="shivamsolutions.com" />
 
-  	<!-- 
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE 
-	DESIGNED & DEVELOPED by FreeHTML5.co
-		
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	-->
-
+ 
   	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
+	<meta property="og:title" content="Jeet Props"/>
 	<meta property="og:image" content=""/>
 	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
+	<meta property="og:site_name" content="Jeet Props"/>
 	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
+	<meta name="twitter:title" content="Setup for films, television,Ad's, events, wedding and many more" />
 	<meta name="twitter:image" content="" />
 	<meta name="twitter:url" content="" />
 	<meta name="twitter:card" content="" />
